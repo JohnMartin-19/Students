0a3a87aa5a756617b190d37cf117e508ff91f502
John Mburu <johnnkonge2020@gmail.com>
2026-06-17 11:35:11 +0300

    Enabled async in main class of the app

--- student-api/src/main/java/com/mburu/student_api/StudentApiApplication.java
+++ student-api/src/main/java/com/mburu/student_api/StudentApiApplication.java
@@ -3,7 +3,8 @@ package com.mburu.student_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication   // @EnableJpaAuditing removed - was raising errors in test — now lives in JpaConfig
+@SpringBootApplication
+@EnableAsync
 public class StudentApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentApiApplication.class, args);

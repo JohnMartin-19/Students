e0e24b78e88f889bb9bafabc0182bac17bf68c8f
John Mburu <johnnkonge2020@gmail.com>
2026-06-18 05:40:23 +0300

    Configuring Scheduling and Async processing

--- student-api/src/main/java/com/mburu/student_api/StudentApiApplication.java
+++ student-api/src/main/java/com/mburu/student_api/StudentApiApplication.java
@@ -2,9 +2,13 @@ package com.mburu.student_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annoataion.EnableScheduling;
+
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class StudentApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentApiApplication.class, args);
